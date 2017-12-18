11327505eafd496ff1f8884a7df559797c4e001d
petermok97 <petermok97@gmail.com>
2017-12-18 18:19:30 +0200

    Added error pages

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,14 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function initDatabase()
+    {
+        Artisan::call('migrate');
+    }
+
+    protected function resetDatabase()
+    {
+        Artisan::call('migrate:reset');
+    }
 }
